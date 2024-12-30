69ac76db3be7b0c86ac6b187536c59307fccb77e
Ryan Lopopolo <rjl@hyperbo.la>
2024-12-30 11:25:05 +0900

    Only drop a `String` during free if the `String` is initialized
    In Ruby, objects are normally created by calling `<Class>.new`, but
    under the covers this does a Smalltalk-style sequence of calling:
    
    ```
    instance = <Class>.allocate
    instance.initialize
    ```
    
    `Object::allocate` is directly callable by application code, and there
    are Ruby Specs which exercise this behavior. This permits a state where
    an `RString*` object is created and live on the GC heap but does not
    contain a byte buffer.
    
    When upgrading to Rust 1.82.0, new pointer operation well-formedness
    assertions from `core` in debug builds were firing in `mrb_gc_free_str`
    when attempting to construct a `Vec` with a null pointer.
    
    To address this source of UB, first check whether the pointer in the
    `RString*` is null by parsing it with `std::ptr::NonNull`. If the `RString*`
    contains a null pointer, that means we have an uninitialized `String` and
    can safely skip dropping a backing buffer.

--- artichoke-backend/src/extn/core/string/ffi.rs
+++ artichoke-backend/src/extn/core/string/ffi.rs
@@ -11,6 +11,7 @@ use std::collections::TryReserveError;
 use std::ffi::{c_void, CStr};
 use std::io::Write as _;
 use std::os::raw::{c_char, c_double, c_int};
+use std::ptr::NonNull;
 
 use artichoke_core::convert::Convert;
 use artichoke_core::hash::Hash as _;
@@ -711,7 +712,11 @@ unsafe extern "C" fn mrb_byte_hash_step(s: *const u8, len: sys::mrb_int, mut hva
 unsafe extern "C" fn mrb_gc_free_str(mrb: *mut sys::mrb_state, string: *mut sys::RString) {
     let _ = mrb;
 
-    let ptr = (*string).as_.heap.ptr;
+    let Some(ptr) = NonNull::<c_char>::new((*string).as_.heap.ptr) else {
+        // An allocated but uninitialized string has no backing byte buffer, so
+        // there is nothing to free.
+        return;
+    };
     let length = (*string).as_.heap.len as usize;
     let capacity = (*string).as_.heap.aux.capa as usize;
 
@@ -719,7 +724,7 @@ unsafe extern "C" fn mrb_gc_free_str(mrb: *mut sys::mrb_state, string: *mut sys:
     // it into the `RString` flags as a `u32`.
 
     let raw_parts = RawParts {
-        ptr: ptr.cast::<u8>(),
+        ptr: ptr.cast::<u8>().as_mut(),
         length,
         capacity,
     };
