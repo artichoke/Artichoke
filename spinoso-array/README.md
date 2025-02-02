dd3220ac7d0903d3108ff98d2fc82bfb819df3ec
Ryan Lopopolo <rjl@hyperbo.la>
2025-02-02 10:07:34 -0800

    Use const generics for `PartialEq` impls on `spinoso-array` vectors
    Bump crate version.

--- spinoso-array/README.md
+++ spinoso-array/README.md
@@ -30,7 +30,7 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-spinoso-array = "0.10.0"
+spinoso-array = "0.11.0"
 ```
 
 Then construct and manipulate an `Array` like this:
--- spinoso-array/src/array/smallvec/eq.rs
+++ spinoso-array/src/array/smallvec/eq.rs
@@ -107,80 +107,42 @@ where
     }
 }
 
-macro_rules! __smallarray_T_eq_primitive_array {
-    ($len:literal) => {
-        impl<T, U> PartialEq<[U; $len]> for SmallArray<T>
-        where
-            T: PartialEq<U>,
-        {
-            #[inline]
-            fn eq(&self, other: &[U; $len]) -> bool {
-                self[..] == other[..]
-            }
-        }
-
-        impl<T, U> PartialEq<SmallArray<U>> for [T; $len]
-        where
-            T: PartialEq<U>,
-        {
-            #[inline]
-            fn eq(&self, other: &SmallArray<U>) -> bool {
-                self[..] == other[..]
-            }
-        }
-
-        impl<T, U> PartialEq<&[U; $len]> for SmallArray<T>
-        where
-            T: PartialEq<U>,
-        {
-            #[inline]
-            fn eq(&self, other: &&[U; $len]) -> bool {
-                self[..] == other[..]
-            }
-        }
-
-        impl<T, U> PartialEq<SmallArray<U>> for &[T; $len]
-        where
-            T: PartialEq<U>,
-        {
-            #[inline]
-            fn eq(&self, other: &SmallArray<U>) -> bool {
-                self[..] == other[..]
-            }
-        }
-    };
+impl<T, U, const N: usize> PartialEq<[U; N]> for SmallArray<T>
+where
+    T: PartialEq<U>,
+{
+    #[inline]
+    fn eq(&self, other: &[U; N]) -> bool {
+        self[..] == other[..]
+    }
 }
 
-__smallarray_T_eq_primitive_array!(0);
-__smallarray_T_eq_primitive_array!(1);
-__smallarray_T_eq_primitive_array!(2);
-__smallarray_T_eq_primitive_array!(3);
-__smallarray_T_eq_primitive_array!(4);
-__smallarray_T_eq_primitive_array!(5);
-__smallarray_T_eq_primitive_array!(6);
-__smallarray_T_eq_primitive_array!(7);
-__smallarray_T_eq_primitive_array!(8);
-__smallarray_T_eq_primitive_array!(9);
-__smallarray_T_eq_primitive_array!(10);
-__smallarray_T_eq_primitive_array!(11);
-__smallarray_T_eq_primitive_array!(12);
-__smallarray_T_eq_primitive_array!(13);
-__smallarray_T_eq_primitive_array!(14);
-__smallarray_T_eq_primitive_array!(15);
-__smallarray_T_eq_primitive_array!(16);
-__smallarray_T_eq_primitive_array!(17);
-__smallarray_T_eq_primitive_array!(18);
-__smallarray_T_eq_primitive_array!(19);
-__smallarray_T_eq_primitive_array!(20);
-__smallarray_T_eq_primitive_array!(21);
-__smallarray_T_eq_primitive_array!(22);
-__smallarray_T_eq_primitive_array!(23);
-__smallarray_T_eq_primitive_array!(24);
-__smallarray_T_eq_primitive_array!(25);
-__smallarray_T_eq_primitive_array!(26);
-__smallarray_T_eq_primitive_array!(27);
-__smallarray_T_eq_primitive_array!(28);
-__smallarray_T_eq_primitive_array!(29);
-__smallarray_T_eq_primitive_array!(30);
-__smallarray_T_eq_primitive_array!(31);
-__smallarray_T_eq_primitive_array!(32);
+impl<T, U, const N: usize> PartialEq<SmallArray<U>> for [T; N]
+where
+    T: PartialEq<U>,
+{
+    #[inline]
+    fn eq(&self, other: &SmallArray<U>) -> bool {
+        self[..] == other[..]
+    }
+}
+
+impl<T, U, const N: usize> PartialEq<&[U; N]> for SmallArray<T>
+where
+    T: PartialEq<U>,
+{
+    #[inline]
+    fn eq(&self, other: &&[U; N]) -> bool {
+        self[..] == other[..]
+    }
+}
+
+impl<T, U, const N: usize> PartialEq<SmallArray<U>> for &[T; N]
+where
+    T: PartialEq<U>,
+{
+    #[inline]
+    fn eq(&self, other: &SmallArray<U>) -> bool {
+        self[..] == other[..]
+    }
+}
