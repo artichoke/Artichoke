500b5888f91b137a49671c33b39b82798ad2e53b
Ryan Lopopolo <rjl@hyperbo.la>
2025-02-03 21:44:10 -0800

    Autogenerate full-coverage unit tests for `spinoso-exception`
    Add `bstr` as a dep for a better `Debug` impl and test assertion
    helpers. Remove `Hash` impl on exception types. Bump crate version.

--- spinoso-exception/src/core/standarderror.rs
+++ spinoso-exception/src/core/standarderror.rs
@@ -6,6 +6,7 @@ use alloc::vec::Vec;
 use core::error;
 use core::fmt;
 
+use bstr::ByteSlice;
 use scolapasta_string_escape::format_debug_escape_into;
 
 use crate::RubyException;
@@ -22,9 +23,17 @@ use crate::RubyException;
 /// [`Exception`]: https://ruby-doc.org/core-3.1.2/Exception.html
 /// [`Kernel#raise`]: https://ruby-doc.org/core-3.1.2/Kernel.html#method-i-raise
 /// [`NameError#name`]: https://ruby-doc.org/core-3.1.2/NameError.html#method-i-name
-#[derive(Default, Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct StandardError {
     message: Cow<'static, [u8]>,
+}
+
+impl fmt::Debug for StandardError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("StandardError")
+            .field("message", &self.message().as_bstr())
+            .finish()
+    }
 }
 
 impl StandardError {
@@ -174,5 +183,180 @@ impl RubyException for StandardError {
     #[inline]
     fn name(&self) -> Cow<'_, str> {
         Cow::Borrowed(Self::name(self))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::borrow::Cow;
+    use alloc::format;
+    use alloc::string::ToString;
+    use core::cmp::Ordering;
+    use core::error;
+
+    use bstr::ByteSlice;
+
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let exception = StandardError::new();
+        assert_eq!(exception.message().as_bstr(), b"StandardError".as_bstr());
+        assert_eq!(exception.name(), "StandardError");
+    }
+
+    #[test]
+    fn test_with_message() {
+        let custom_message = "custom message";
+        let exception = StandardError::with_message(custom_message);
+        assert_eq!(exception.message().as_bstr(), custom_message.as_bytes().as_bstr());
+        // The exception name remains the default even when a custom message is used.
+        assert_eq!(exception.name(), "StandardError");
+    }
+
+    #[test]
+    fn test_from_string() {
+        let message = "from String".to_string();
+        let exception: StandardError = message.into();
+        assert_eq!(exception.message().as_bstr(), b"from String".as_bstr());
+    }
+
+    #[test]
+    fn test_from_static_str() {
+        let message: &'static str = "from &'static str";
+        let exception: StandardError = message.into();
+        assert_eq!(exception.message().as_bstr(), b"from &'static str".as_bstr());
+    }
+
+    #[test]
+    fn test_from_cow_str_borrowed() {
+        let cow: Cow<'static, str> = Cow::Borrowed("from Cow borrowed");
+        let exception: StandardError = cow.into();
+        assert_eq!(exception.message().as_bstr(), b"from Cow borrowed".as_bstr());
+    }
+
+    #[test]
+    fn test_from_cow_str_owned() {
+        let cow: Cow<'static, str> = Cow::Owned("from Cow owned".to_string());
+        let exception: StandardError = cow.into();
+        assert_eq!(exception.message().as_bstr(), b"from Cow owned".as_bstr());
+    }
+
+    #[test]
+    fn test_from_vec_u8() {
+        let vec = b"from Vec<u8>".to_vec();
+        let exception: StandardError = vec.into();
+        assert_eq!(exception.message().as_bstr(), b"from Vec<u8>".as_bstr());
+    }
+
+    #[test]
+    fn test_from_static_slice() {
+        let slice: &'static [u8] = b"from &'static [u8]";
+        let exception: StandardError = slice.into();
+        assert_eq!(exception.message().as_bstr(), b"from &'static [u8]".as_bstr());
+    }
+
+    #[test]
+    fn test_from_cow_u8_borrowed() {
+        let cow: Cow<'static, [u8]> = Cow::Borrowed(b"from Cow<u8> borrowed");
+        let exception: StandardError = cow.into();
+        assert_eq!(exception.message().as_bstr(), b"from Cow<u8> borrowed".as_bstr());
+    }
+
+    #[test]
+    fn test_from_cow_u8_owned() {
+        let cow: Cow<'static, [u8]> = Cow::Owned(b"from Cow<u8> owned".to_vec());
+        let exception: StandardError = cow.into();
+        assert_eq!(exception.message().as_bstr(), b"from Cow<u8> owned".as_bstr());
+    }
+
+    #[test]
+    fn test_debug() {
+        let exception = StandardError::with_message("display test");
+        let output = format!("{exception:?}");
+        // Expected to contain the exception name and the debug-escaped message.
+        assert!(output.contains("StandardError"));
+        assert!(output.contains("display test"));
+        // Check the surrounding formatting.
+        assert!(output.starts_with("StandardError {"));
+        assert!(output.ends_with('}'));
+    }
+
+    #[test]
+    fn test_display() {
+        let exception = StandardError::with_message("display test");
+        let output = format!("{exception}");
+        // Expected to contain the exception name and the debug-escaped message.
+        assert!(output.contains("StandardError"));
+        assert!(output.contains("display test"));
+        // Check the surrounding formatting.
+        assert!(output.starts_with("StandardError ("));
+        assert!(output.ends_with(')'));
+    }
+
+    #[test]
+    fn test_error_trait() {
+        let exception = StandardError::with_message("error trait test");
+        let error_obj: &dyn error::Error = &exception;
+        // Our implementation does not provide a source.
+        assert!(error_obj.source().is_none());
+        // to_string() uses Display.
+        assert_eq!(error_obj.to_string(), format!("{exception}"));
+    }
+
+    #[test]
+    fn test_ruby_exception_trait() {
+        let exception = StandardError::with_message("ruby trait test");
+        let msg = RubyException::message(&exception);
+        let name = RubyException::name(&exception);
+        assert_eq!(msg.as_bstr(), exception.message().as_bstr());
+        assert_eq!(name, exception.name());
+    }
+
+    #[test]
+    fn test_default() {
+        let default_error = StandardError::default();
+        // By default, the message should be an empty slice since Default is auto-derived.
+        assert_eq!(default_error.message(), b"");
+        // The name method always returns the constant exception name.
+        assert_eq!(default_error.name(), "StandardError");
+    }
+
+    #[test]
+    fn test_clone() {
+        let original = StandardError::with_message("clone test");
+        let cloned = original.clone();
+        assert_eq!(original, cloned, "Cloned error should be equal to the original");
+    }
+
+    #[test]
+    fn test_partial_eq() {
+        let error_a = StandardError::with_message("test message");
+        let error_b = StandardError::with_message("test message");
+        let error_c = StandardError::with_message("different message");
+        assert_eq!(error_a, error_b, "Errors with the same message should be equal");
+        assert_ne!(error_a, error_c, "Errors with different messages should not be equal");
+    }
+
+    #[test]
+    fn test_partial_ord() {
+        let error_a = StandardError::with_message("aaa");
+        let error_b = StandardError::with_message("bbb");
+        // Same error should compare equal.
+        assert_eq!(error_a.partial_cmp(&error_a), Some(Ordering::Equal));
+        // Lexicographic ordering of the underlying messages.
+        assert_eq!(error_a.partial_cmp(&error_b), Some(Ordering::Less));
+        assert_eq!(error_b.partial_cmp(&error_a), Some(Ordering::Greater));
+    }
+
+    #[test]
+    fn test_ord() {
+        let error_a = StandardError::with_message("aaa");
+        let error_b = StandardError::with_message("bbb");
+        // Same error should compare equal.
+        assert_eq!(error_a.cmp(&error_a), Ordering::Equal);
+        // Lexicographic ordering of the underlying messages.
+        assert_eq!(error_a.cmp(&error_b), Ordering::Less);
+        assert_eq!(error_b.cmp(&error_a), Ordering::Greater);
     }
 }
